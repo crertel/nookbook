41a8133692c37811d08004d73a52b25297e07eb0
Chris Ertel <>
2020-09-02 14:25:27 -0500

    step 13

---
+++ config/releases.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :nook_book, NookBookWeb.Endpoint,
+  server: true,
+  http: [port: 4000],
+  url: [host: "localhost"],
+  secret_keybase: "05RTlIyB3HMbNpEeINxIAV3rOcEFHAivnniKMbqCD9i6NRD/XGquZJNt60kxsQR1"
